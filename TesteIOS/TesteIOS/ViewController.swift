cfe765a7f84a7a07fa529c186513148c83b91f38
Jorge Mendes <jorge.mendes@amaris.com>
2016-05-25 18:07:11 +0100

    initial commit

---
+++ TesteIOS/TesteIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TesteIOS
+//
+//  Created by Jorge Mendes on 25/05/16.
+//  Copyright © 2016 amaris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
